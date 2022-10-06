6e190ec4d2dcd4098769457f3cf9b273097247b3
agumartinez1209 <agu.am.2359@gmail.com>
2022-10-05 23:43:57 -0300

    agrego

--- src/main.js
+++ src/main.js
@@ -1,71 +1,152 @@
-import express from 'express'
-import session from 'express-session'
-import MongoStore from 'connect-mongo'
-
-import config from './config.js'
-
-import { Server as HttpServer } from 'http'
-import { Server as Socket } from 'socket.io'
-
-import authWebRouter from './routers/web/auth.js'
-import homeWebRouter from './routers/web/home.js'
-import productosApiRouter from './routers/api/productos.js'
-
-import addProductosHandlers from './routers/ws/productos.js'
-import addMensajesHandlers from './routers/ws/mensajes.js'
+import express from "express";
+import { Server as HttpServer } from "http";
+import { Server as Socket } from "socket.io";
+import ContenedorSQL from "./contenedores/ContenedorSQL.js";
+import config from "./config.js";
+import * as fakeProdApi from "./api/fakeProds.js";
+import MongoDbContainer from "./contenedores/ContenedorMongoDB.js";
+import * as msgsConfig from "./config/msgs.js";
+import * as msgNormalizer from "./utils/normalizer.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 //--------------------------------------------
 // instancio servidor, socket y api
 
-const app = express()
-const httpServer = new HttpServer(app)
-const io = new Socket(httpServer)
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new Socket(httpServer);
+
+const productosApi = new ContenedorSQL(config.mariaDb, "productos");
+const mensajesApi = new MongoDbContainer(
+  msgsConfig.msgsCollection,
+  msgsConfig.msgsSchema
+);
 
 //--------------------------------------------
 // configuro el socket
 
-io.on('connection', async socket => {
-    // console.log('Nuevo cliente conectado!');
-    addProductosHandlers(socket, io.sockets)
-    addMensajesHandlers(socket, io.sockets)
+const processMsgData = (msgData) => {
+    const plainMsgs = msgData.map((msg) => {
+      const dateTime = new Date(parseInt(msg.id.substring(0, 8), 16) * 1000);
+      delete msg.author["_id"];
+      delete msg["__v"];
+      msg = { ...msg, dateTime };
+      return msg;
+    });
+    const originalData = { id: "mensajes", mensajes: plainMsgs };
+    return messageNormalizr.getNormalized(originalData)
+  };
+  import util from "util"
+  io.on("connection", async (socket) => {
+    
+    const productos = await productosApi.getAll();
+    io.sockets.emit("productos", productos);
+    const msgData = await mensajesApi.getAll();
+    const mensajes = processMsgData(msgData)
+    io.sockets.emit("mensajes", mensajes);
+  
+    console.log("Nueva conexion");
+    
+    socket.on("newProduct", async (data) => {
+      await productosApi.guardar(data);
+      const productos = await productosApi.getAll();
+      io.sockets.emit("productos", productos);
+    });
+  
+    
+    socket.on("newMessage", async (data) => {
+      await mensajesApi.createNew(data);
+      const msgData = await mensajesApi.getAll();
+      const mensajes = processMsgData(msgData)
+      io.sockets.emit("mensajes", mensajes);
+    });
+  });
+
+//--------------------------------------------
+// agrego middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// setteo sesiones
+const sessionStore = MongoStore.create({
+  mongoUrl:
+    "mongodb+srv://agumartinez1209:<password>@cluster0.druz5we.mongodb.net/testdesafio?retryWrites=true&w=majority",
+  ttl: 60,
 });
 
-//--------------------------------------------
-// configuro el servidor
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
-
-app.set('view engine', 'ejs');
-
-app.use(session({
-    // store: MongoStore.create({ mongoUrl: config.mongoLocal.cnxStr }),
-    store: MongoStore.create({ mongoUrl: config.mongoRemote.cnxStr }),
-    secret: 'shhhhhhhhhhhhhhhhhhhhh',
+app.use(
+  session({
+    store: sessionStore,
+    secret: "sessionSecret",
     resave: false,
     saveUninitialized: false,
-    rolling: true,
-    cookie: {
-        maxAge: 60000
+  })
+);
+
+//Set engine
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+const isLoggedIn = (req, res, next) => {
+  if (!req.session.nombre) return res.redirect("/login");
+  next();
+};
+// si estoy no estoy loggeado llama next, sino hace un redirect al home
+const isLoggedOut = (req, res, next) => {
+  if (req.session.nombre) return res.redirect("/");
+  next();
+};
+
+// rutas
+
+app.get("/login", isLoggedOut, (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", isLoggedOut, (req, res) => {
+  if (req.body.nombre) {
+    req.session.nombre = req.body.nombre;
+    res.redirect("/");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/", isLoggedIn, (req, res) => {
+  res.render("index", { nombre: req.session.nombre });
+});
+
+app.get("/api/productos-test", (req, res) => {
+  const fakeProds = fakeProdApi.generateMany(5);
+  res.send(fakeProds);
+});
+
+
+app.get("/logout", isLoggedIn, (req, res) => {
+  // cargo temporalmente el nombre de la sesion
+  const nombre = req.session.nombre;
+  req.session.destroy((err) => {
+    if (err) {
+      res.json({ status: "Logout Error", body: err });
+    } else {
+      // uso el nombre de la sesion
+      res.render("logout", { nombre: nombre });
     }
-}))
-
-//--------------------------------------------
-// rutas del servidor API REST
-
-app.use(productosApiRouter)
-
-//--------------------------------------------
-// rutas del servidor web
-
-app.use(authWebRouter)
-app.use(homeWebRouter)
+  });
+});
 
 //--------------------------------------------
 // inicio el servidor
 
-const connectedServer = httpServer.listen(config.PORT, () => {
-    console.log(`Servidor http escuchando en el puerto ${connectedServer.address().port}`)
-})
-connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
+const PORT = 8080;
+const connectedServer = httpServer.listen(PORT, () => {
+  console.log(
+    `Servidor http escuchando en el puerto ${connectedServer.address().port}`
+  );
+});
+connectedServer.on("error", (error) =>
+  console.log(`Error en servidor ${error}`)
+);
